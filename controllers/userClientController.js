69f05100891c82ee37fc6e824cedf6a08c825348
Natayha-Raicoff <natayharaicoff4@gmail.com>
2025-05-09 20:37:27 -0300

    terminamos los 4 controles
    finalizamos los controllers de las 4 entidades, dichas entidades son: Category, ClientUsers, AdminUsers, Product. nos resta una ultima entidad, esta es determinada de la siguiente manera: Order.
    
    Co-Authored-By: MaraAnima <179888074+MaraAnima@users.noreply.github.com>
    Co-Authored-By: SarabOlivera <187664642+SarabOlivera@users.noreply.github.com>

--- controllers/userClientController.js
+++ controllers/userClientController.js
@@ -34,10 +34,23 @@ async function store(req, res) {
 }
 
 // Update the specified resource in storage.
-async function update(req, res) {}
+async function update(req, res) {
+  const updateClient = await ClientUser.findByPk(req.params.id);
+  await updateClient.update({
+    name: req.body.name,
+    lastname: req.body.lastname,
+    email: req.body.email,
+    address: req.body.address,
+    password: req.body.password
+  })
+   res.json(updateClient)
+}
 
 // Remove the specified resource from storage.
-async function destroy(req, res) {}
+async function destroy(req, res) {
+  await ClientUser.destroy({ where: {id: req.params.id}});
+res.send("Se ha eliminado correctamente")
+}
 
 // Otros handlers...
 // ...
